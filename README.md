d7f5083ba91f800243b7b2f7d230c8820b3d1bcd
THEBESTol0ch <105137450+THEBESTol0ch@users.noreply.github.com>
2023-06-25 23:55:24 +1000

    added bot mainframe

---
+++ README.md
@@ -0,0 +1,9 @@
+## QueriBot»
+<a href="https://codeclimate.com/github/THEBESTol0ch/QueriBot/maintainability"><img src="https://api.codeclimate.com/v1/badges/cf7979cbcffa44012ec5/maintainability" /></a>
+
+## Description:
+```
+Discord bot for an IT college project. Is a nice addition to the discord server, it is able to answer the user's questions.
+```
+
+![Bot Logo](https://media.discordapp.net/attachments/1117444086077595752/1122509779210870854/1.png?width=468&height=468)
---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const { Client, IntentsBitField } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+console.log("POWERING ON");
+client.login(process.env.TOKEN);
+client.on("ready", (c) => {
+    console.log("ONLINE");
+})
+
+client.on("messageCreate", (message) => {
+    if (message.author.bot) {
+        return;
+    }
+
+    message.reply(message.content);
+});
+
+client.on("interactionCreate", (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName == "a_hey") {
+        interaction.reply("hey!");
+    }
+    if (interaction.commandName == "a_ping") {
+        interaction.reply("pong!");
+    }
+});
